add6a319697b23d0450fb3c315c769cbdf5d78b8
nqwzxs <nqwzxs@gmail.com>
2023-03-19 19:33:02 +0500

    Add changeColor function

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const grid = document.querySelector("#grid");
+let squares = grid.childNodes;
 
 function createGrid() {
-    for (let i = 0; i < 32; i++) {
+    for (let i = 0; i < 16 * 16; i++) {
         square = document.createElement("div");
         grid.appendChild(square);    
     }
 }
 
+function changeColor(e) {
+    e.target.classList.add("colored");
+}
+
 createGrid();
+
+squares.forEach((square) => {
+    square.addEventListener("mouseover", changeColor);
+});
