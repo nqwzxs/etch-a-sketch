79d468ba558f773e9246bd05b7a9faa8ce702651
nqwzxs <nqwzxs@gmail.com>
2023-03-20 08:02:55 +0500

    Add change-size button and changeSize function

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 const grid = document.querySelector("#grid");
-let squares = grid.childNodes;
+const squares = grid.childNodes;
+const changeSizeButton = document.querySelector("#change-size")
 
-function createGrid() {
-    for (let i = 0; i < 16 * 16; i++) {
+let numberOfSquares = 16;
+
+function createGrid(numberOfSquares) {
+    grid.style["grid-template-columns"] = `repeat(${numberOfSquares}, 1fr)`;
+    grid.style["grid-template-rows"] = `repeat(${numberOfSquares}, 1fr)`;
+    
+    for (let i = 0; i < numberOfSquares * numberOfSquares; i++) {
         square = document.createElement("div");
+        square.addEventListener("mouseover", changeColor);
         grid.appendChild(square);    
     }
+}
+
+function removeGrid() {
+    grid.innerHTML = ''
 }
 
 function changeColor(e) {
     e.target.classList.add("colored");
 }
 
-createGrid();
+function changeSize() {
+    numberOfSquares = +prompt("Choose a number of squares per side up to 100:", 16)
+    removeGrid();
+    createGrid(numberOfSquares);
+}
 
-squares.forEach((square) => {
-    square.addEventListener("mouseover", changeColor);
-});
+createGrid(numberOfSquares);
+
+changeSizeButton.addEventListener("click", changeSize);
