ea848daf6762b93a36291804869e400c169c485f
nqwzxs <nqwzxs@gmail.com>
2023-03-20 08:33:57 +0500

    Add limit to size and style button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const grid = document.querySelector("#grid");
 const squares = grid.childNodes;
 const changeSizeButton = document.querySelector("#change-size")
 
+let color = "black";
 let numberOfSquares = 16;
 
 function createGrid(numberOfSquares) {
@@ -20,13 +21,31 @@ function removeGrid() {
 }
 
 function changeColor(e) {
-    e.target.classList.add("colored");
+    e.target.style["background-color"] = color;
+}
+
+function getNumber() {
+    input = +prompt("Choose a number of squares per side up to 100:", 16)
+    console.log(input);
+
+    if (input > 0 && input < 100) {
+        return input;
+    } else if (input === 0) {
+        return 16;
+    } else {
+        alert("Wrong input. Try Again.");
+    }
 }
 
 function changeSize() {
-    numberOfSquares = +prompt("Choose a number of squares per side up to 100:", 16)
+    let number;
+
+    while (!number) {
+        number = getNumber();
+    }
+
     removeGrid();
-    createGrid(numberOfSquares);
+    createGrid(number);
 }
 
 createGrid(numberOfSquares);
